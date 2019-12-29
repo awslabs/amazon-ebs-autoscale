cb002a2fd16974dfec3c488bbac6f8007acf7904
W. Lee Pang <wleepang@gmail.com>
2019-12-29 14:01:29 -0800

    fix typo in folder name

--- templates/upstart/install.sh
+++ templates/upstart/install.sh
@@ -3,9 +3,6 @@
 # install the upstart config
 sed -e "s#YOUR_MOUNTPOINT#${MOUNTPOINT}#" ebs-autoscale.conf.template > /etc/init/ebs-autoscale.conf
 
-# install the logrotate config
-cp ebs-autoscale.logrotate /etc/logrotate.d/ebs-autoscale
-
 # Register the ebs-autoscale upstart conf and start the service
 initctl reload-configuration
 initctl start ebs-autoscale
