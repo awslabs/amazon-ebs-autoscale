14ed106fea2d312ec64f70dd5f433b2e9131e1b5
Zvika Gart <zvikagart@gmail.com>
2023-02-07 17:50:36 +0000

    fix: imdsv2 loaded from config file

--- shared/utils.sh
+++ shared/utils.sh
@@ -32,19 +32,19 @@ function get_metadata() {
     local key=$1
     local metadata_ip='169.254.169.254'
 
-    if [ ! -z "$IMDSV2" ]; then
+    if [ ! -z "$(get_config_value .imdsv2)" ]; then
         local token=$(curl -s -X PUT "http://$metadata_ip/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 60")
         local token_wrapper='-H "X-aws-ec2-metadata-token: $token"'
     fi
     
-    echo `curl -s $token_wrapper http://$metadata_ip/latest/meta-data/$key`
+    eval "curl -s $token_wrapper http://$metadata_ip/latest/meta-data/$key"
 }
 
 function initialize() {
+    export EBS_AUTOSCALE_CONFIG_FILE=/etc/ebs-autoscale.json
     export AWS_AZ=$(get_metadata placement/availability-zone)
     export AWS_REGION=$(echo ${AWS_AZ} | sed -e 's/[a-z]$//')
     export INSTANCE_ID=$(get_metadata instance-id)
-    export EBS_AUTOSCALE_CONFIG_FILE=/etc/ebs-autoscale.json
 }
 
 function detect_init_system() {
